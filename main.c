dd5ca8e5b89d384fcba5f393f2744c764ab1ec0b
BeginArjun <jayshrma161@gmail.com>
2025-01-01 22:25:44 +0530

    Enhance search functionality with navigation support and improved state management

--- main.c
+++ main.c
@@ -421,16 +421,36 @@ void editorSave(){
 /*** find ***/
 
 void editorFindCallback(char *query, int key){
+    static int last_match = -1; // -1 if no match found else row number
+    static int direction = 1; // 1 for forward, -1 for backward
+
     if(key == '\r' || key == '\x1b'){
+        last_match = -1;
+        direction = 1;
         return;
+    }else if(key == ARROW_RIGHT || key == ARROW_DOWN){
+        direction = 1;
+    }else if(key == ARROW_LEFT || key == ARROW_UP){
+        direction = -1;
+    }else{
+        last_match = -1;
+        direction = 1;
     }
+
+    if(last_match == -1) direction = 1;
+    int current = last_match;
 
     int i;
     for(i = 0; i < E.numrows; i++){
-        erow *row = &E.row[i];
+        current += direction;
+        if(current == -1) current = E.numrows - 1;
+        else if(current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if(match){
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxtoCx(row, match - row->render);
             E.rowoffset = E.numrows;
             break;
@@ -439,10 +459,20 @@ void editorFindCallback(char *query, int key){
 }
 
 void editorFind(){
-    char *query = editorPrompt(" Search: %s (ESC to cancel)", editorFindCallback);
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloffset = E.coloffset;
+    int saved_rowoffset = E.rowoffset;
+    
+    char *query = editorPrompt(" Search: %s (ESC to cancel/ Arrows to Move/ Enter to Confirm)", editorFindCallback);
 
     if(query){
         free(query);
+    }else{
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloffset = saved_coloffset;
+        E.rowoffset = saved_rowoffset;
     }
 }
 
